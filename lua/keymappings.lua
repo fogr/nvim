4f350c311bbf53e7a3ada1aaa9a609171b136e33
tramoc <mw.fogr@gmail.com>
2022-06-25 16:29:55 +0700

    feat(lsp): add lua klsp

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -1,57 +1,57 @@
 -- Leader key
-vim.api.nvim_set_keymap('n', '<Space>', '<NOP>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<Space>', '<NOP>', { noremap = true, silent = true })
 vim.g.mapleader = ' '
 
 local wk = require('which-key')
 
 wk.register({
   r = {
-    r = {':source $MYVIMRC', 'Reload neovim config'},
+    r = { ':source $MYVIMRC<cr>', 'Reload neovim config' },
   }
-  }, { mode = 'n', prefix = '<leader>', noremap = true, silent = true })
+}, { mode = 'n', prefix = '<leader>', noremap = true, silent = true })
 
-  --# Window movement
-  -- Tree
-  wk.register({
-    ['<leader>'] = {
-      e = {':NvimTreeToggle<CR>', 'Toggle tree view'},
-    }
-  }, { mode = 'n', noremap = true, silent = true })
+--# Window movement
+-- Tree
+wk.register({
+  ['<leader>'] = {
+    e = { ':NvimTreeToggle<CR>', 'Toggle tree view' },
+  }
+}, { mode = 'n', noremap = true, silent = true })
 
-  -- Switch window 
-  wk.register({
-    h = { '<C-w>h', 'Switch to left window' },
-    l = { '<C-w>l', 'Switch to right window' },
-    j = { '<C-w>j', 'Switch to bottom window' },
-    k = { '<C-w>k', 'Switch to top window' },
-  }, { prefix = 'C', mode = 'n', silent = true })
+-- Switch window
+wk.register({
+  h = { '<C-w>h', 'Switch to left window' },
+  l = { '<C-w>l', 'Switch to right window' },
+  j = { '<C-w>j', 'Switch to bottom window' },
+  k = { '<C-w>k', 'Switch to top window' },
+}, { prefix = 'C', mode = 'n', silent = true })
 
-  -- Switch tab
-  wk.register({
-    ['<TAB>'] = { ':bnext<CR>', 'Switch to next buffer' },
-    ['<S-TAB>'] = { ':bnext<CR>', 'Switch to previous buffer' },
-  }, { mode = 'n', noremap = true, silent = true })
+-- Switch tab
+wk.register({
+  ['<TAB>'] = { ':bnext<CR>', 'Switch to next buffer' },
+  ['<S-TAB>'] = { ':bnext<CR>', 'Switch to previous buffer' },
+}, { mode = 'n', noremap = true, silent = true })
 
 
-  -- Text editing
-  wk.register({
-    v = {
-      name = "Indentation",
-      ['<'] = {'<gv', 'Snap left'},
-      ['>'] = {'>gv', 'Snap right'},
-    },
-  }, { mode = 'v', noremap = true, silent = true})
-  wk.register({
-    x = {
-      name = "Move selected",
-      J = {":move '<-2<CR>gv-gv", 'Move selected line up'},
-      K = {":move '>+1<CR>gv-gv", 'Move selected line down'},
-    }
-  }, { mode = 'x', noremap = true, silent = true})
+-- Text editing
+wk.register({
+  v = {
+    name = "Indentation",
+    ['<'] = { '<gv', 'Snap left' },
+    ['>'] = { '>gv', 'Snap right' },
+  },
+}, { mode = 'v', noremap = true, silent = true })
+wk.register({
+  x = {
+    name = "Move selected",
+    J = { ":move '<-2<CR>gv-gv", 'Move selected line up' },
+    K = { ":move '>+1<CR>gv-gv", 'Move selected line down' },
+  }
+}, { mode = 'x', noremap = true, silent = true })
 
 -- TAB complete
 wk.register({
-  ['<expr><TAB>'] = {'pumvisible() ? "\\<C-n>": "\\<TAB>"', 'Tab complete'},
+  ['<expr><TAB>'] = { 'pumvisible() ? "\\<C-n>": "\\<TAB>"', 'Tab complete' },
 }, { mode = 'x', noremap = true, silent = true })
 
 
@@ -59,23 +59,22 @@ wk.register({
 wk.register({
   f = {
     name = "File",
-    f = {':Telescope find_files<CR>', 'Find files'},
-    g = {':Telescope live_grep<CR>', 'Find grep'},
-    b = {':Telescope buffers<CR>', 'List buffers'},
-    h = {':Telescope help_tags<CR>', 'Show help'},
+    f = { ':Telescope find_files<CR>', 'Find files' },
+    g = { ':Telescope live_grep<CR>', 'Find grep' },
+    b = { ':Telescope buffers<CR>', 'List buffers' },
+    h = { ':Telescope help_tags<CR>', 'Show help' },
   },
   g = {
     name = "Git",
-    b = {':Telescope git_branches<CR>', 'List branches'},
-    g = {':LazyGit<CR>', 'Lazy git'},
+    b = { ':Telescope git_branches<CR>', 'List branches' },
+    g = { ':LazyGit<CR>', 'Lazy git' },
   },
   b = {
     name = "Debug",
-    c = {':Telescope dap commands<CR>', 'DAP commands'},
-    s = {':Telescope dap configurations<CR>', 'DAP configurations'},
-    l = {':Telescope dap list_breakpoints<CR>', 'DAP breakpoints'},
-    v = {':Telescope dap variables<CR>', 'DAP variables'},
-    f = {':Telescope dap frames<CR>', 'DAP frames'},
+    c = { ':Telescope dap commands<CR>', 'DAP commands' },
+    s = { ':Telescope dap configurations<CR>', 'DAP configurations' },
+    l = { ':Telescope dap list_breakpoints<CR>', 'DAP breakpoints' },
+    v = { ':Telescope dap variables<CR>', 'DAP variables' },
+    f = { ':Telescope dap frames<CR>', 'DAP frames' },
   }
-}, { mode = 'n', prefix='<leader>', noremap = true, silent = true })
-
+}, { mode = 'n', prefix = '<leader>', noremap = true, silent = true })
--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,20 +1,22 @@
 require 'lsp.tsserver-rc'
 require 'lsp.gopls-rc'
+require 'lsp.lua-rc'
 
 local nvim_lsp = require('lspconfig')
 local wk = require('which-key')
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
+local on_attach = function(_, bufnr)
   local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+
   local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
   --Enable completion triggered by <c-x><c-o>
   buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
   -- Mappings.
-  local opts = { noremap=true, silent=true }
+  local opts = { noremap = true, silent = true }
 
   local fn = function(func, api)
     api = api or 'buf'
@@ -24,7 +26,7 @@ local on_attach = function(client, bufnr)
   -- See `:help vim.lsp.*` for documentation on any of the below functions
   wk.register({
     ['<leader>'] = {
-      e = {':NvimTreeToggle<CR>', 'Toggle tree view'},
+      e = { ':NvimTreeToggle<CR>', 'Toggle tree view' },
     }
   }, { buffer = bufnr, mode = 'n', noremap = true, silent = true })
 
@@ -46,47 +48,34 @@ local on_attach = function(client, bufnr)
       c = {
         name = 'Code',
         n = { fn('rename'), 'Rename' },
-        a = { fn('code_action'), 'Code action'},
+        a = { fn('code_action'), 'Code action' },
         s = { fn('signature_help'), 'Signature help' },
-        f = { fn('formatting'), 'Formatting'},
+        f = { fn('formatting'), 'Formatting' },
       },
       l = {
-        e = { fn('show_line_diagnostics', 'diagnostic'), 'Show line diagnostic'},
-        l = { fn('set_loclist', 'diagnostic'), 'Set loclist'},
+        e = { fn('show_line_diagnostics', 'diagnostic'), 'Show line diagnostic' },
+        l = { fn('set_loclist', 'diagnostic'), 'Set loclist' },
         n = { fn('goto_next', 'diagnostic'), 'Next diagnostic' },
         p = { fn('goto_prev', 'diagnostic'), 'Previous diagnostic' },
       }
     },
-    ['<C-h>'] = { fn('hover')},
+    ['<C-h>'] = { fn('hover') },
   }, { mode = 'n', noremap = true, silent = true })
-  -- buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  -- buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  -- buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  -- buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  -- buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  -- buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  -- buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  -- buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  -- buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  -- buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  -- buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  -- buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  -- buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-  -- buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-  -- buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-  -- buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-  -- buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-
 end
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { "gopls", "tsserver" }
+local servers = {
+  "gopls",
+  "tsserver",
+  "sumneko_lua",
+}
+
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {
     on_attach = on_attach,
     flags = {
-      debounce_text_changes = 150,
+      debounce_text_changes = 200,
     }
   }
 end
